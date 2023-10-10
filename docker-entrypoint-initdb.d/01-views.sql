640fa256ae57858663de0036d3ec2b81b4a76b11
B. K. Oxley (binkley) <binkley@alumni.rice.edu>
2023-10-10 07:56:03 -0500

    Formatting

--- docker-entrypoint-initdb.d/01-views.sql
+++ docker-entrypoint-initdb.d/01-views.sql
@@ -3,23 +3,25 @@ AS
 SELECT key                                          AS KEY,
        value                                        AS VALUE,
        COALESCE(
-               CASE WHEN paragraph_id IS NOT NULL THEN 'PARAGRAPH' ELSE NULL END,
-               CASE WHEN sentence_id IS NOT NULL THEN 'SENTENCE' ELSE NULL END,
-               CASE WHEN word_id IS NOT NULL THEN 'WORD' ELSE NULL END
+           CASE WHEN paragraph_id IS NOT NULL THEN 'PARAGRAPH' ELSE NULL END,
+           CASE WHEN sentence_id IS NOT NULL THEN 'SENTENCE' ELSE NULL END,
+           CASE WHEN word_id IS NOT NULL THEN 'WORD' ELSE NULL END
        )                                            AS TEXT_TYPE,
        COALESCE(paragraph_id, sentence_id, word_id) AS TEXT_ID
 FROM quotation_p
+-- One way to performantly ensure read-only without using roles
 UNION ALL SELECT '0', 0, '0', 0 WHERE 1 = 0;
 
 CREATE VIEW quotation_expanded
 AS
-SELECT key AS KEY,
-       value AS VALUE,
+SELECT key           AS KEY,
+       value         AS VALUE,
        p.indentation AS INDENTATION,
-       s.clauses AS CLAUSES,
-       w.length AS LENGTH
+       s.clauses     AS CLAUSES,
+       w.length      AS LENGTH
 FROM quotation_p q
 LEFT JOIN paragraph p ON p.id = q.paragraph_id
 LEFT JOIN sentence s ON s.id = q.sentence_id
 LEFT JOIN word w ON w.id = q.word_id
+-- One way to performantly ensure read-only without using roles
 UNION ALL SELECT '0', 0, 0, 0, 0 WHERE 1 = 0;
