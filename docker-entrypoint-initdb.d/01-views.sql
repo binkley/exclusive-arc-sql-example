fa390000b10cb1e8c3561c87962f1278a676d0a9
Brian Oxley <boxley@thoughtworks.com>
2023-10-11 12:50:56 -0500

    Build simpler view on fuller view

--- docker-entrypoint-initdb.d/01-views.sql
+++ docker-entrypoint-initdb.d/01-views.sql
@@ -1,21 +1,13 @@
-CREATE VIEW quotation
-AS
-SELECT key                                          AS KEY,
-       value                                        AS VALUE,
-       COALESCE(
-           CASE WHEN paragraph_id IS NOT NULL THEN 'PARAGRAPH' ELSE NULL END,
-           CASE WHEN sentence_id IS NOT NULL THEN 'SENTENCE' ELSE NULL END,
-           CASE WHEN word_id IS NOT NULL THEN 'WORD' ELSE NULL END
-       )                                            AS TEXT_TYPE,
-       COALESCE(paragraph_id, sentence_id, word_id) AS TEXT_ID
-FROM quotation_p
--- One way to performantly ensure read-only without using roles
-UNION ALL SELECT '0', 0, '0', 0 WHERE 1 = 0;
-
 CREATE VIEW quotation_expanded
 AS
 SELECT key           AS KEY,
        value         AS VALUE,
+       COALESCE(
+           CASE WHEN paragraph_id IS NOT NULL THEN 'PARAGRAPH' ELSE NULL END,
+           CASE WHEN sentence_id IS NOT NULL THEN 'SENTENCE' ELSE NULL END,
+           CASE WHEN word_id IS NOT NULL THEN 'WORD' ELSE NULL END
+       )             AS TEXT_TYPE,
+       COALESCE(paragraph_id, sentence_id, word_id) AS TEXT_ID,
        p.indentation AS INDENTATION,
        s.clauses     AS CLAUSES,
        w.length      AS LENGTH
@@ -24,4 +16,9 @@ LEFT JOIN paragraph p ON p.id = q.paragraph_id
 LEFT JOIN sentence s ON s.id = q.sentence_id
 LEFT JOIN word w ON w.id = q.word_id
 -- One way to performantly ensure read-only without using roles
-UNION ALL SELECT '0', 0, 0, 0, 0 WHERE 1 = 0;
+UNION ALL SELECT '0', 0, '0', 0, 0, 0, 0 WHERE 1 = 0;
+
+CREATE VIEW quotation
+AS
+SELECT KEY, VALUE, TEXT_TYPE, TEXT_ID
+FROM quotation_expanded;
